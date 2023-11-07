e72e7882f68cd52decedddecc91c4f142354e81c
Allan Young <ayounggtr@gmail.com>
2023-11-07 07:12:35 -0500

    Operators Exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           var a = 17;
+           var b = 4;
+           var quotient = a / b;
+           var remainder = a % b;
+
+            //String Concatonation
+            Console.WriteLine(a + " / " + b + " is " + quotient + " remainder " + remainder);
+            //String Interpolation
+            //Console.WriteLine($"{a} / {b} is  {quotient} remainder  {remainder}");
+            Console.WriteLine("Please enter the radius of your circle:");
+            
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+           Console.WriteLine(AreaOfCircle(radius));
+
         }
+
+
+        public static double AreaOfCircle(double radius)
+
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
+
+        
+        }
+
+
+
     }
 }
